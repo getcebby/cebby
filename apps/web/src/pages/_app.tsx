342b7f7b89c8f2c8b89fe2ee3fb5155c5f12804d
Dorell James <galangdj@gmail.com>
2024-11-02 19:38:45 +0800

    chore: updated favicon

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,17 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import TopBanner from "@/components/TopBanner";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div
-      className={`${
-        process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-      }`}
-    >
-      <TopBanner />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <div
+        className={`${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
+      >
+        <TopBanner />
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
