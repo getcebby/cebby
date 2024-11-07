20c37b26e8207eb81e47d789477134f7a886b129
Dorell James <galangdj@gmail.com>
2024-11-07 10:27:34 +0800

    chore: remove redirects

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,21 +4,6 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "**.fbcdn.net" }],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "events.dorelljames.dev",
-          },
-        ],
-        destination: "https://getcebby.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
