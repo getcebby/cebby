ede1cd7b47c3213c80ebb591d9f9b0fd9d4a9ba1
Arnelle Balane <arnellebalane@gmail.com>
2024-12-01 15:01:09 -0800

    chore(eslint): Update eslint-config package to eslint v9 flat configs format

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -1,34 +1,16 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
+import globals from 'globals';
+import { config as baseConfig } from './base.js';
 
 /** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
-  overrides: [
+export const config = [
+    ...baseConfig,
     {
-      files: ["*.js?(x)", "*.ts?(x)"],
+        languageOptions: {
+            sourceType: 'module',
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.node,
+            },
+        },
     },
-  ],
-};
+];
