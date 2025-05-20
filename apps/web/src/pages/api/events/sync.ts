69f6bead447dcd52d0b6b02ffd3cb374be149d1c
Dorell James <galangdj@gmail.com>
2025-05-20 11:37:37 +0800

    chore: updated with better logging

--- apps/web/src/pages/api/events/sync.ts
+++ apps/web/src/pages/api/events/sync.ts
@@ -1,74 +1,67 @@
-import createClient from "@/utils/supabase/api";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { fetchEvent } from "@/utils/fetchEvent";
-import { findJsonInString } from "@/utils/json";
-import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import axios from 'axios';
+import createClient from '@/utils/supabase/api';
+import { fetchEvent } from '@/utils/fetchEvent';
+import { findJsonInString } from '@/utils/json';
 
 type Data = {
-  message?: string;
+    message?: string;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  if (req.method !== "POST") {
-    return res.status(400).json({ message: "Method NOT allowed!" });
-  }
-
-  try {
-    const supabase = createClient(req, res);
-    const { data: fbPages } = await supabase
-      .from("facebook_pages")
-      .select("*")
-      .eq("is_active", true);
-
-    console.log("🚀 ~ fbPages:", fbPages);
-    if (!fbPages) {
-      return res.status(404).json({ message: "No Facebook pages found!" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    if (req.method !== 'POST') {
+        return res.status(400).json({ message: 'Method NOT allowed!' });
     }
 
-    const urls: string[] = await getUrlsFromFbPages(fbPages);
-    if (urls.length === 0) {
-      return res.status(404).json({ message: "No events found" });
+    try {
+        const supabase = createClient(req, res);
+        const { data: fbPages } = await supabase.from('facebook_pages').select('*').eq('is_active', true);
+
+        console.log('🚀 ~ fbPages:', fbPages);
+        if (!fbPages) {
+            return res.status(404).json({ message: 'No Facebook pages found!' });
+        }
+
+        const urls: string[] = await getUrlsFromFbPages(fbPages);
+        if (urls.length === 0) {
+            return res.status(404).json({ message: 'No events found' });
+        }
+
+        const uniqueUrls = Array.from(new Set(urls.flat()))?.filter(Boolean);
+        console.log('🚀 ~ uniqueUrls:', uniqueUrls);
+
+        for (const url of uniqueUrls) {
+            // Send a request to add the event to the database
+            axios
+                .post(`${process.env.NEXT_PUBLIC_API_URL}/api/events/add_by_url`, {
+                    url,
+                })
+                .then(() => console.log(`Initiated adding event from ${url}`));
+        }
+
+        res.status(200).json({ message: 'Synced events!' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Error syncing events' });
     }
-
-    const uniqueUrls = Array.from(new Set(urls.flat()))?.filter(Boolean);
-    console.log("🚀 ~ uniqueUrls:", uniqueUrls);
-
-    for (const url of uniqueUrls) {
-      // Send a request to add the event to the database
-      axios
-        .post(`${process.env.NEXT_PUBLIC_API_URL}/api/events/add_by_url`, {
-          url,
-        })
-        .then(() => console.log(`Initiated adding event from ${url}`));
-    }
-
-    res.status(200).json({ message: "Synced events!" });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: "Error syncing events" });
-  }
 }
 
 interface FbPage {
-  id: string;
-  username: string;
-  url: string;
+    id: string;
+    username: string;
+    url: string;
 }
 
 async function getUrlsFromFbPages(fbPages: FbPage[]) {
-  return Promise.all(
-    fbPages.map(async (page) => {
-      const data = await fetchEvent(page.url);
-      const { jsonData } = findJsonInString(data, "pageItems");
-      console.log("🚀 ~ jsonData:", jsonData?.edges);
+    return Promise.all(
+        fbPages.map(async (page) => {
+            const data = await fetchEvent(page.url);
+            const { jsonData } = findJsonInString(data, 'pageItems');
+            console.log('🚀 ~ jsonData page:', page.username, jsonData?.edges);
 
-      const urls = jsonData?.edges?.map(
-        (item: { node: { url: string } }) => item.node.url
-      );
-      return urls;
-    })
-  );
+            const urls = jsonData?.edges?.map((item: { node: { url: string } }) => item.node.url);
+            return urls;
+        }),
+    );
 }
